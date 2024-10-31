b271fd0cc38e2ac41a02288bdd1bbaa454522d4b
Tami <tamigaertner@outlook.com>
2024-10-31 16:59:51 -0400

    installed dependencies, and created task.py model

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,11 @@
 from sqlalchemy.orm import Mapped, mapped_column
 from ..db import db
+from typing import Optional
+from datetime import datetime
 
 class Task(db.Model):
+    __tablename__ ="task_list_api_development"
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    title: Mapped[str]
+    description: Mapped[str]
+    completed_at: Mapped[Optional[datetime]] = mapped_column(null=True, default=None)
